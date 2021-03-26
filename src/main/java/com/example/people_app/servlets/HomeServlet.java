74ecac26df8f7b8fcc655b876aba79bef5b163ea
Stas Grigoryev <stasilius2009@gmail.com>
2021-03-26 15:14:04 +0300

    Initial commit #7

--- src/main/java/com/example/people_app/servlets/HomeServlet.java
+++ src/main/java/com/example/people_app/servlets/HomeServlet.java
@@ -7,12 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home")
+@WebServlet("/")
 public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.getRequestDispatcher("/views/home.html").forward(req, resp);
     }
 
     @Override
